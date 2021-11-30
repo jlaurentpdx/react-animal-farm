db6ac170aa44066af20a126695d95e03ebe4ee02
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-11-29 18:05:38 -0800

    initialized .map function in Main to store Animal props

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './Main.css'
+import { animals } from '../../data.js'
+import Animal from '../Animal/Animal.js'
+
 import background from '../../../src/background.png'
 
 export default function Main() {
   return (
-    <main>
-      <img src={background} alt="background" />
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.type}
+          name={animal.name}
+          type={animal.type}
+          says={animal.says}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
     </main>
   )
 }
